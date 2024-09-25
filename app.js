312d8abaf3b69b4ed53d241a37611c7f12205446
Anik <65917000+xyryc@users.noreply.github.com>
2024-09-25 19:02:40 +0600

    seat functionality ie. validation, alert, coupon activation

--- app.js
+++ app.js
@@ -1,21 +1,45 @@
 const selectedSeatEl = document.getElementById("selected-seat");
 const totalBookedEl = document.getElementById("total-booked");
+const availableSeatEl = document.getElementById("available-seat");
+const totalPriceEl = document.getElementById("total-price");
+const couponInputEl = document.getElementById("coupon-field");
+const couponBtnEl = document.getElementById("coupon-btn");
 
 let selectedSeat = [];
+let totalPrice = 0;
 
 function handleSelectSeat(event) {
-  console.log(event.innerText);
+  const value = event.innerText;
 
-  event.classList.add("bg-primary");
-  event.classList.add("text-white");
-  event.classList.remove("hover:bg-secondary");
+  if (selectedSeat.includes(value)) {
+    return alert("Seat Already Booked!");
+  } else if(selectedSeat.length < 4) {
+    event.classList.add("bg-primary", "hover:bg-primary", "text-white");
+    event.classList.remove("hover:bg-secondary");
 
-  selectedSeat.push(event.innerText);
-  totalBookedEl.innerText = selectedSeat.length
+    selectedSeat.push(event.innerText);
+    totalBookedEl.innerText = selectedSeat.length;
 
-    selectedSeatEl.innerHTML = `<li class="text-base font-normal flex justify-between">
-          <span>${event.innerText}</span>
-          <span>Economy</span>
-          <span>550</span>
-      </li>`;
+    const availableSeatValue = parseFloat(availableSeatEl.innerText);
+    const newAvailableSeatValue = availableSeatValue - 1;
+    availableSeatEl.innerText = newAvailableSeatValue;
+
+    selectedSeatEl.innerHTML += `<li class="text-base font-normal flex justify-between">
+            <span>${event.innerText}</span>
+            <span>Economy</span>
+            <span>550</span>
+        </li>`;
+
+    // update total price
+    totalPrice += 550;
+    totalPriceEl.innerText = totalPrice.toFixed(2);
+
+    // active coupon button
+    if (selectedSeat.length > 3) {
+      couponInputEl.removeAttribute("disabled");
+      couponBtnEl.removeAttribute("disabled");
+    }
+  }else{
+    return alert("Maximum Seat Booked!")
+  }
 }
